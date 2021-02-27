12b6650415b350ce3478802fe9bc8c9afd9e29ee
hamza15 <sardan5@vt.edu>
2021-02-27 02:42:09 -0500

    commit tree 2eb10e34b51645e200627c21b7ce99438aece2fa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(arr, fn) {
+    return arr.map(element => fn(element))
+}
+
+function reduce(arr, cb, start=0) {
+    let r = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < arr.length; i++) {
+      r = cb(arr[i], r)
+    }
+  
+    return r;
+}
